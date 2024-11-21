fef215e17dd00fce66b0ba282913b2e518107325
Eduardo Massarani <edmassarani@gmail.com>
2024-11-21 21:58:35 +0100

    basic logic for filtering options

--- autocomplete/src/App.vue
+++ autocomplete/src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
 import AutoComplete from './components/AutoComplete.vue'
+import { useBooksStore, useCitiesStore } from './stores'
+
+const bookStore = useBooksStore()
+const citiesStore = useCitiesStore()
 </script>
 
 <template>
-  <header>
-    <div class="flex items-center justify-center gap-4 px-2 py-4">
-      <h1 class="text-2xl font-bold">Simple Autocomplete</h1>
-      <img class="size-8" alt="Vue logo" src="./assets/logo.svg" />
+  <main class="flex h-full flex-col justify-center">
+    <div class="-mt-16 mb-8 flex items-center justify-center gap-4 px-2 py-4">
+      <h1 class="text-4xl font-bold">Simple Autocomplete</h1>
+      <img class="size-10" alt="Vue logo" src="./assets/logo.svg" />
     </div>
-  </header>
 
-  <main class="flex flex-col items-center gap-4">
-    <AutoComplete placeholder="Search cities..." class="w-96" />
-    <AutoComplete placeholder="Search books..." class="w-96" />
+    <div class="flex flex-col items-center justify-center gap-4">
+      <AutoComplete class="w-96" placeholder="Search cities..." :options="citiesStore.cities" />
+      <AutoComplete
+        class="w-96"
+        placeholder="Search books..."
+        :options="bookStore.books"
+        filter-prop="title"
+      />
+    </div>
   </main>
 </template>
 
--- autocomplete/src/components/AutoComplete.vue
+++ autocomplete/src/components/AutoComplete.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
-defineProps<{
+import { computed, ref } from 'vue'
+
+interface Props {
   placeholder: string
-}>()
+  options: string[] | { [key: string]: string }[]
+  filterProp?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  placeholder: 'Type to search...',
+  options: () => [],
+  filterProp: undefined,
+})
+
+const inputText = ref('')
+
+const displayedOptions = computed(() => {
+  if (inputText.value.length < 3) return []
+
+  return props.options.filter((option) => {
+    let value = ''
+
+    if (typeof option === 'string') {
+      value = option
+    } else {
+      value = option[props.filterProp ?? ''] ?? ''
+    }
+
+    return value.toLocaleLowerCase().includes(inputText.value.toLocaleLowerCase())
+  })
+})
 </script>
 
 <template>
   <div>
     <input
+      v-model="inputText"
       class="w-full rounded border px-3 py-2 outline-none"
       type="text"
       :placeholder="placeholder"
